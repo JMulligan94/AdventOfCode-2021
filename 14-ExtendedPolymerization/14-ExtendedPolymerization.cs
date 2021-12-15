91fdc52781295b98ace5f9b6b4cd458a90819910
JMulligan94 <j.mulligan94@hotmail.co.uk>
2021-12-15 14:32:20 +0000

    Day 14 (Part 1), Day 15 - Completed

---
+++ 14-ExtendedPolymerization/14-ExtendedPolymerization.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _14_ExtendedPolymerization
+{
+	class Program
+	{
+		static string PerformStep(string template, ref char[,] rules)
+		{
+			string newTemplate = template;
+
+			List<Tuple<int, char>> toInsert = new List<Tuple<int, char>>();
+			for(int i = 0; i < template.Length-1; ++i)
+			{
+				char thisChar = template[i];
+				char nextChar = template[i+1];
+				char insertionChar = rules[thisChar - 'A', nextChar - 'A'];
+				if (insertionChar != 0)
+				{
+					toInsert.Add(new Tuple<int, char>(i+1, insertionChar));
+				}
+			}
+
+			// Iterate backwards and insert new chars (since it is done simultaneously)
+			for (int i = toInsert.Count - 1; i >= 0; --i)
+			{
+				int insertIndex = toInsert[i].Item1;
+				string insertChar = toInsert[i].Item2 + "";
+
+				newTemplate = newTemplate.Insert(insertIndex, insertChar);
+				//Console.WriteLine("\tInserted " + insertChar + " at " + insertIndex);
+			}
+
+			return newTemplate;
+		}
+
+		static UInt64 GetQuantityRange(string template)
+		{
+			UInt64[] charCounts = new UInt64[26];
+			foreach (var character in template)
+			{
+				charCounts[character - 'A']++;
+			}
+
+			UInt64 highestCount = UInt64.MinValue;
+			int highestIndex = 0;
+			UInt64 lowestCount = UInt64.MaxValue;
+			int lowestIndex = 0;
+
+			for (int countIndex = 0; countIndex < charCounts.Length; ++countIndex)
+			{
+				UInt64 count = charCounts[countIndex];
+
+				if (count == 0) // Character is not included in template
+					continue;
+
+				if (highestCount < count)
+				{
+					highestCount = count;
+					highestIndex = countIndex;
+				}
+
+				if (lowestCount > count)
+				{
+					lowestCount = count;
+					lowestIndex = countIndex;
+				}
+			}
+
+			Console.WriteLine("Most common = " + (char)(highestIndex + 'A') + " (" + highestCount + " times)");
+			Console.WriteLine("Least common = " + (char)(lowestIndex + 'A') + " (" + lowestCount + " times)");
+
+			return highestCount - lowestCount;
+		}
+
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("test.txt");
+
+			string initialTemplate = lines[0];
+			List<Tuple<string, string>> stringInsertionRules = new List<Tuple<string, string>>();
+
+			for (int lineIndex = 2; lineIndex < lines.Length; ++lineIndex)
+			{
+				var ruleTokens = lines[lineIndex].Split(' ');
+				stringInsertionRules.Add(new Tuple<string, string>(ruleTokens[0], ruleTokens[2]));
+			}
+
+			char[,] insertionRules = new char[26, 26];
+
+			foreach (var rule in stringInsertionRules)
+			{
+				int row = (rule.Item1[0] - 'A');
+				int col = (rule.Item1[1] - 'A');
+				insertionRules[row, col] = rule.Item2[0];
+			}
+
+			string template = initialTemplate;
+			Console.WriteLine("Template:\t\t" + template);
+
+			// Perform 10 steps
+			for (int i = 0; i < 10; ++i)
+			{
+				template = PerformStep(template, ref insertionRules);
+				//Console.WriteLine("Completed step " + i);
+				Console.WriteLine("After step " + i + ":\t\t" + template);
+			}
+
+			// Part One
+			{
+				Console.WriteLine("\nPart One:");
+
+				// Get the difference between most common char and least common
+				UInt64 quantityDiff = GetQuantityRange(template);
+				Console.WriteLine("Quantity difference after 10 steps is: " + quantityDiff + "\n");
+			}
+
+			// Perform 30 more steps - 40 total
+			for (int i = 10; i < 40; ++i)
+			{
+				template = PerformStep(template, ref insertionRules);
+				Console.WriteLine("Completed step " + i);
+				//Console.WriteLine("After step " + i + ":\t\t" + template);
+			}
+
+			// Part Two
+			{
+				Console.WriteLine("\nPart Two:");
+
+				// Get the difference between most common char and least common
+				UInt64 quantityDiff = GetQuantityRange(template);
+				Console.WriteLine("Quantity difference after 40 steps is: " + quantityDiff + "\n");
+			}
+		}
+	}
+}
---
+++ 15-Chiton/15-Chiton.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _15_Chiton
+{
+	class Map
+	{
+		public class Node
+		{
+			public int row;
+			public int col;
+			public int cost;
+			public bool visited; // Has this node been visited?
+
+			public int score;	 // The score for the distance between this node and the start node so far
+			public Node scoreFrom; // The node that the above score was from
+
+			public bool partOfOptimalPath; // Is this node part of the optimal path between start and end nodes
+
+			public Node(int _row, int _col, int _cost)
+			{
+				row = _row;
+				col = _col;
+				cost = _cost;
+
+				score = int.MaxValue;
+				visited = false;
+			}
+
+			public override string ToString()
+			{
+				return "(" + row + "," + col + "): " + cost;
+			}
+		}
+
+		int height;
+		int width;
+		Node[,] nodes;
+
+		Node startNode;
+		Node goalNode;
+
+		public Map(int _height, int _width, int[,] riskLevels)
+		{
+			height = _height;
+			width = _width;
+
+			nodes = new Node[height, width];
+			for(int row = 0; row < height; ++row)
+			{
+				for (int col = 0; col < width; ++col)
+				{
+					nodes[row, col] = new Node(row, col, riskLevels[row, col]);
+				}
+			}
+		}
+
+		public void SetStart(int row, int col)
+		{
+			nodes[row, col].visited = true;
+			nodes[row, col].score = 0;
+			startNode = nodes[row, col];
+		}
+
+		public void SetGoal(int row, int col)
+		{
+			goalNode = nodes[row, col];
+		}
+
+		private void CheckNeighbour(Node current, ref Node neighbour, ref List<Node> unvisitedNodes)
+		{
+			// Score it has taken to get to this node
+			int newScore = neighbour.cost + current.score;
+
+			// If the way we got here was faster than what the node has experienced before
+			// Overwrite the data in it so its linked to this current one
+			if (neighbour.score > newScore)
+			{
+				neighbour.score = newScore;
+				neighbour.scoreFrom = current;
+			}
+
+			// And if the neighbour hasn't been checked before, add it into the list of nodes to visit in the future
+			if (!neighbour.visited)
+			{
+				if (!unvisitedNodes.Contains(neighbour))
+				{
+					unvisitedNodes.Add(neighbour);
+				}
+			}
+		}
+
+		public void FindPath(ref List<Node> path)
+		{
+			// List of nodes that we haven't checked yet (ordered by score from start node)
+			List<Node> unvisitedNodes = new List<Node>();
+			unvisitedNodes.Add(startNode);
+
+			while (unvisitedNodes.Count > 0)
+			{
+				// Grab the cheapest node from the start so far
+				Node currentNode = unvisitedNodes[0];
+
+				// If we've hit the end node, we've found the fastest path!
+				if (currentNode == goalNode)
+					break;
+
+				int row = currentNode.row;
+				int col = currentNode.col;
+
+				// Check each neighbour
+				if (row != 0)
+				{
+					// North neighbour
+					Node northNode = nodes[row-1, col];
+					CheckNeighbour(currentNode, ref northNode, ref unvisitedNodes);
+				}
+
+				if (row != height-1)
+				{
+					// South neighbour
+					Node southNode = nodes[row+1, col];
+					CheckNeighbour(currentNode, ref southNode, ref unvisitedNodes);
+				}
+
+				if (col != 0)
+				{
+					// West neighbour
+					Node westNode = nodes[row, col-1];
+					CheckNeighbour(currentNode, ref westNode, ref unvisitedNodes);
+				}
+				
+
+				if (col != width-1)
+				{
+					// East neighbour
+					Node eastNode = nodes[row, col+1];
+					CheckNeighbour(currentNode, ref eastNode, ref unvisitedNodes);
+				}
+
+				// Now we've checked this nodes neighbours, mark this as visited so we don't add it back to the list of nodes to check
+				currentNode.visited = true;
+
+				// Remove from list of nodes
+				unvisitedNodes.RemoveAt(0);
+
+				// Sort the array of unvisited nodes by the score, so we're grabbing the shortest path next time
+				unvisitedNodes.Sort((x, y) => x.score.CompareTo(y.score));
+			}
+
+			// Trace back from goalNode to determine path
+			path = new List<Node>();
+			Node previousNode = goalNode;
+			while (previousNode != null)
+			{
+				path.Insert(0, previousNode);
+				previousNode.partOfOptimalPath = true;
+				previousNode = previousNode.scoreFrom;
+			}
+		}
+
+		public void PrintMap()
+		{
+			Console.WriteLine("=== Map ===");
+
+			for (int row = 0; row < height; ++row)
+			{
+				string rowStr = "";
+				for (int col = 0; col < width; ++col)
+				{
+					rowStr += nodes[row, col].partOfOptimalPath
+						? "[" + nodes[row, col].cost + "]"
+						: "." + nodes[row, col].cost + ".";
+				}
+				Console.WriteLine(rowStr);
+			}
+		}
+	}
+
+	class Program
+	{
+		// Dijkstra's Algorithm = https://www.redblobgames.com/pathfinding/a-star/introduction.html
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("input.txt");
+			
+			// Part One - Find optimal path cost from top left to bottom right on input
+			{
+				int height = lines.Length;
+				int width = lines[0].Length;
+
+				// Parse input
+				int[,] costs = new int[lines.Length, lines[0].Length];
+				for (int row = 0; row < lines.Length; ++row)
+				{
+					for (int col = 0; col < lines[row].Length; ++col)
+					{
+						costs[row, col] = int.Parse(lines[row][col] + "");
+					}
+				}
+
+				// Create map of nodes
+				Map riskLevelMap = new Map(height, width, costs);
+
+				// Set start and goal nodes
+				riskLevelMap.SetStart(0, 0);
+				riskLevelMap.SetGoal(height - 1, width - 1);
+
+				// Find shortest path
+				List<Map.Node> path = new List<Map.Node>();
+				riskLevelMap.FindPath(ref path);
+
+				riskLevelMap.PrintMap();
+
+				Console.WriteLine("Part One:");
+				Console.WriteLine("The cost of the path is: " + path.Last().score);
+			}
+
+			// Part Two - Find optimal path from top left to bottom right using input as a 1x1 tile in a 5x5 grid
+			// where risk increases per tile
+			{
+				int tileHeight = lines.Length;
+				int tileWidth = lines[0].Length;
+
+				int height = tileHeight * 5;
+				int width = tileWidth * 5;
+
+				int[,] costs = new int[height, width];
+
+				// Grow 5x5 map from single 1x1 tile horizontally while reading in tile
+				for (int row = 0; row < tileHeight; ++row)
+				{
+					for (int col = 0; col < tileWidth; ++col)
+					{
+						int risk = int.Parse(lines[row][col] + "");
+						for (int i = 0; i < 5; ++i)
+						{
+							costs[row, (tileWidth * i) + col] = risk;
+
+							risk++;
+							if (risk > 9)
+								risk = 1; // Wrap around to 1 once we hit 10
+						}
+					}
+				}
+
+				// Grow 5x5 map from single 1x1 tile vertically
+				for (int col = 0; col < width; ++col)
+				{
+					for (int row = 0; row < tileHeight; ++row)
+					{
+						int risk = costs[row,col];
+						for (int i = 1; i < 5; ++i)
+						{
+							risk++;
+							if (risk > 9)
+								risk = 1; // Wrap around to 1 once we hit 10
+
+							costs[row + (tileHeight * i), col] = risk;
+						}
+					}
+				}
+
+				Map riskLevelMap = new Map(height, width, costs);
+				riskLevelMap.PrintMap();
+
+				// Set start and goal nodes
+				riskLevelMap.SetStart(0, 0);
+				riskLevelMap.SetGoal(height - 1, width - 1);
+
+				// Find shortest path
+				List<Map.Node> path = new List<Map.Node>();
+				riskLevelMap.FindPath(ref path);
+
+				riskLevelMap.PrintMap();
+
+				Console.WriteLine("\nPart Two:");
+				Console.WriteLine("The cost of the path is: " + path.Last().score);
+			}
+		}
+	}
+}
