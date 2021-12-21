2f72f57267536d28bc74088a85cfe0f883b88bf0
JMulligan94 <j.mulligan94@hotmail.co.uk>
2021-12-21 13:58:50 +0000

    Day 20 - Completed

---
+++ 19-BeaconScanner/19-BeaconScanner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _19_BeaconScanner
+{
+	using IntVec3 = Tuple<int,int,int>;
+
+	class Scanner
+	{
+		public int id;
+		public List<IntVec3> relativeBeaconLocations = new List<IntVec3>();
+	}
+
+	class Program
+	{
+		static void GenerateAllOrientations(IntVec3 vectorIn, ref List<IntVec3> vectorOut)
+		{
+			vectorOut.Add(vectorIn);															// x,y,z
+			vectorOut.Add(new IntVec3(vectorIn.Item1, vectorIn.Item3, -vectorIn.Item2));		// x,z,-y
+			vectorOut.Add(new IntVec3(vectorIn.Item1, -vectorIn.Item2, -vectorIn.Item3));		// x,-y,-z
+			vectorOut.Add(new IntVec3(vectorIn.Item1, -vectorIn.Item3, vectorIn.Item2));		// x,-z,y
+
+			vectorOut.Add(new IntVec3(-vectorIn.Item1, -vectorIn.Item2, -vectorIn.Item3));		// -x,-y,-z
+			vectorOut.Add(new IntVec3(-vectorIn.Item1, vectorIn.Item3, -vectorIn.Item2));		// -x,z,-y
+			vectorOut.Add(new IntVec3(-vectorIn.Item1, -vectorIn.Item2, -vectorIn.Item3));		// -x,-y,-z
+			vectorOut.Add(new IntVec3(-vectorIn.Item1, -vectorIn.Item3, vectorIn.Item2));		// -x,-z,y
+
+		}
+
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("testA.txt");
+
+			List<Scanner> scanners = new List<Scanner>();
+			bool newScannerInfo = true;
+			Scanner currentScanner = null;
+			foreach (var line in lines)
+			{
+				if (newScannerInfo)
+				{
+					currentScanner = new Scanner();
+					var idLineTokens = line.Split(' ');
+					currentScanner.id = int.Parse(idLineTokens[2]);
+					newScannerInfo = false;
+
+					scanners.Add(currentScanner);
+					continue;
+				}
+				if (line == "")
+				{
+					newScannerInfo = true;
+					continue;
+				}
+				else
+				{
+					var tokens = line.Split(',');
+					currentScanner.relativeBeaconLocations.Add(new IntVec3(int.Parse(tokens[0]), int.Parse(tokens[1]), 0));
+				}
+			}
+			int i = 0;
+
+			IntVec3 vectorIn = new IntVec3(2, 3, 1);
+
+			List<IntVec3> orientations = new List<IntVec3>();
+			GenerateAllOrientations(vectorIn, ref orientations);
+			foreach(var orientation in orientations)
+			{
+				Console.WriteLine("Orientation: (" + orientation.Item1 + "," + orientation.Item2 + "," + orientation.Item3 + ")");
+			}
+
+		}
+	}
+}
---
+++ 20-TrenchMap/20-TrenchMap.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _20_TrenchMap
+{
+	class Image
+	{
+		List<string> pixels = new List<string>();
+		
+		int width = 0;
+		int height = 0;
+
+		int timesEnhanced = 0;		// The amount of times this image has been enhanced
+		char infinitePixel = '.';	// The current state of all infinite pixels
+
+		public void AddPixels(string pixelLine)
+		{
+			pixels.Add(pixelLine);
+
+			width = pixelLine.Length;
+			height = pixels.Count;
+		}
+
+		public void EnhanceImage(string enhancementAlgorithm)
+		{
+			// Grow image by 2 in all directions (since image is infinite)
+			{
+				int newWidth = width + 2;
+				int newHeight = height + 2;
+
+				// Grow using the current state of the infinite pixels
+				string emptyLine = "";
+				for (int i = 0; i < newWidth; ++i)
+					emptyLine += infinitePixel;
+
+				// Add left and right pixels
+				for (int i = 0; i < height; ++i)
+					pixels[i] = infinitePixel + pixels[i] + infinitePixel;
+
+				// Add top and bottom line
+				pixels.Insert(0, emptyLine);
+				pixels.Add(emptyLine);
+
+				width = newWidth;
+				height = newHeight;
+			}
+
+			List<List<int>> enhancementIndices = new List<List<int>>();
+			// Check each pixel to get enhancement indices
+			{
+				for(int row = 0; row < height; ++row)
+				{
+					enhancementIndices.Add(new List<int>());
+					for (int col = 0; col < width; ++col)
+					{
+						enhancementIndices[row].Add(CalculateEnhancementIndex(row, col));
+					}
+				}
+			}
+
+			// Enhance pixels from indices calculated simultaneously
+			{
+				for (int row = 0; row < height; ++row)
+				{
+					string newLine = "";
+					for (int col = 0; col < width; ++col)
+					{
+						newLine += enhancementAlgorithm[enhancementIndices[row][col]];
+					}
+					pixels[row] = newLine;
+				}
+			}
+
+			timesEnhanced++;
+
+			// Flip the infinite pixels to their inverse states if needed
+			// (i.e. if all surrounding pixels are unlit, we take index 0 -> lit pixel
+			//   and if all surrounding pixels are lit, we take index 255 -> unlit pixel)
+			if (enhancementAlgorithm[0] == '#' && enhancementAlgorithm[255] == '.')
+				infinitePixel = timesEnhanced % 2 != 0 ? '#' : '.';
+		}
+
+		public int CalculateEnhancementIndex(int row, int col)
+		{
+			// Create binary string from surrounding pixels and convert to int index for the enhancement algorithm
+			string binaryString = "";
+
+			bool isTop = row == 0;
+			bool isLeft = col == 0;
+			bool isBottom = row == pixels.Count - 1;
+			bool isRight = col == pixels[0].Length - 1;
+
+			char infiniteBit = infinitePixel == '#' ? '1' : '0';
+
+			// Top left
+			bool hasTopLeft = !isTop && !isLeft;
+			if (hasTopLeft)
+				binaryString += pixels[row - 1][col - 1] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Top
+			bool hasTop = !isTop;
+			if (hasTop)
+				binaryString += pixels[row - 1][col] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Top right
+			bool hasTopRight = !isTop && !isRight;
+			if (hasTopRight)
+				binaryString += pixels[row - 1][col + 1] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Left
+			bool hasLeft = !isLeft;
+			if (hasLeft)
+				binaryString += pixels[row][col-1] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Middle
+			binaryString += pixels[row][col] == '#' ? "1" : "0";
+
+			// Right
+			bool hasRight = !isRight;
+			if (hasRight)
+				binaryString += pixels[row][col+1] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Bottom left
+			bool hasBottomLeft = !isBottom && !isLeft;
+			if (hasBottomLeft)
+				binaryString += pixels[row+1][col - 1] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Bottom
+			bool hasBottom = !isBottom;
+			if (hasBottom)
+				binaryString += pixels[row+1][col] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			// Bottom right
+			bool hasBottomRight = !isBottom && !isRight;
+			if (hasBottomRight)
+				binaryString += pixels[row+1][col+1] == '#' ? "1" : "0";
+			else
+				binaryString += infiniteBit;
+
+			return Convert.ToInt32(binaryString, 2);
+		}
+
+		public int GetNumLitPixels()
+		{
+			int numberLit = 0;
+			for (int row = 0; row < height; ++row)
+			{
+				for (int col = 0; col < width; ++col)
+				{
+					if (pixels[row][col] == '#')
+						numberLit++;
+				}
+			}
+			return numberLit;
+		}
+
+		public void PrintImage()
+		{
+			Console.WriteLine("\n==== Image ====");
+			foreach (var line in pixels)
+			{
+				string output = "";
+				foreach (var pixel in line)
+				{
+					output += pixel + " ";
+				}
+				Console.WriteLine(output);
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("input.txt");
+			string enhancementAlgorithm = lines[0];
+
+			// Part One
+			{
+				Console.WriteLine("\nPart One:");
+				Image image = new Image();
+				for (int i = 2; i < lines.Length; ++i)
+				{
+					image.AddPixels(lines[i]);
+				}
+
+				Console.WriteLine("\nBefore:");
+				image.PrintImage();
+
+				// Enhance twice
+				image.EnhanceImage(enhancementAlgorithm);
+				image.EnhanceImage(enhancementAlgorithm);
+
+				Console.WriteLine("\nAfter:");
+				image.PrintImage();
+
+				Console.WriteLine("Number of lit pixels after 2 enhancements is: " + image.GetNumLitPixels());
+			}
+
+			// Part Two
+			{
+				Console.WriteLine("\nPart Two:");
+				Image image = new Image();
+				for (int i = 2; i < lines.Length; ++i)
+				{
+					image.AddPixels(lines[i]);
+				}
+
+				Console.WriteLine("\nBefore:");
+				image.PrintImage();
+
+				// Enhance 50 times
+				for (int i = 0; i < 50; ++i)
+				{
+					image.EnhanceImage(enhancementAlgorithm);
+				}
+				Console.WriteLine("\nAfter:");
+				image.PrintImage();
+
+				Console.WriteLine("Number of lit pixels after 50 enhancements is: " + image.GetNumLitPixels());
+			}
+		}
+	}
+}
